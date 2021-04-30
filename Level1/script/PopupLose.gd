80567f1bbc3e8d04a308a2dbf29bd937a09bcde3
ErinLiutr <erinliu618@outlook.com>
2021-04-30 17:09:17 -0500

    update

--- Level1/script/PopupLose.gd
+++ Level1/script/PopupLose.gd
@@ -4,14 +4,16 @@ var pressed = false
 
 func _ready():
 	set_physics_process(true)
-	set_process_unhandled_key_input(false)
+	set_process_unhandled_key_input(true)
 
 func _unhandled_key_input(event):
 	if event.is_action_pressed("ui_interact"):
+		print("pressed")
 		pressed = true
 	elif event.is_action_released("ui_interact"):
+		print("none")
 		pressed = false
-
+	
 func _physics_process(delta):
 	if get_parent().get_parent().get_node("Password").lose:
 		if pressed:
